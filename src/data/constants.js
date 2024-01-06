3f7e85879ab0d1c592f169c11f0e4c3defee8788
Shujauddin Qadri <mshuja.uq@gmail.com>
2024-01-06 18:04:11 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,8 +8,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/gh-shujauddin",
-  resume:
-    "https://gh-shujauddin.github.io/my-resume/",
+  resume: "https://drive.google.com/file/d/1vTkcm4NONvMqZtWzWIQsZvs6A3lgvSIV/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/shqadri/",
   twitter: "https://twitter.com/sh__qadri",
   insta: "https://www.instagram.com/qadri.81/"
