dbee51aabedb263632bab7fb97e19bd3df72c50f
Shujauddin Qadri <mshuja.uq@gmail.com>
2024-02-21 18:47:49 +0530

    Update jpg with png

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Bio } from '../../data/constants';
 import Typewriter from 'typewriter-effect';
-import HeroImg from '../../images/HeroImage.jpg';
+import HeroImg from '../../images/HeroImage.png';
 import HeroBgAnimation from '../HeroBgAnimation';
 
 const HeroContainer = styled.div`
